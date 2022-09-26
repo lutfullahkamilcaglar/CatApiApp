f682346cf3fdcfc9cdf558eb2a36281502efe8a8
kamilcaglar <alarkamil34@gmail.com>
2022-09-26 15:30:04 +0200

    first commit

---
+++ CatApiApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CatApiApp
+//
+//  Created by Kamil Caglar on 26/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let urlString = "https://api.thecatapi.com/v1/images/search?api_key=live_JxOOMGfIT1WVQSAUOzyUcR9OtwDaih5Tt7ShLFb5f4dxK493UOFDcpAmot1mLx4i"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
